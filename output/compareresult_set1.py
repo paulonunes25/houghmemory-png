42c4f5c33e38867fba7da505d0f8842452b6c9fd
Paulo Nunes <pulonunes@mail.com>
2016-08-08 16:30:09 +0100

    new output with zzuf noise and computed performance

---
+++ output/compareresult_set1.py
@@ -0,0 +1,30 @@
+from __future__ import division
+
+first = 0
+amount = 26
+threshold = 4
+
+for i in range(first,first + amount):
+	positionname = 'position_rnd{:04d}.txt'.format(i)
+	print positionname
+	p = open(positionname, 'r')
+	ctrlset = []
+	for line in p:
+		tmp = line.split('=')
+		ctrlset = ctrlset + [ int(tmp[1]) ]
+
+	for j in range(0, 17):
+		tp = 0
+		fp = 0
+		dumpname =  'export_noise{:02d}_dump_rnd{:04d}.dat'.format(j, i)
+		print "   " + dumpname
+		f = open(dumpname, 'r')
+		for line in f:
+			count = line.split(';')
+			if int(count[1]) >= threshold:
+				if int(count[0]) in ctrlset:
+					tp = tp + 1
+				else:
+					fp = fp + 1
+		f.close()	
+		print '      Control = {:d}, TP = {:d}, FP = {:d}'.format(len(ctrlset), tp, fp)
